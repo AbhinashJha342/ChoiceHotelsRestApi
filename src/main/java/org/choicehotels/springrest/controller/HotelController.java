e5fda3e07d559abdfa6a60c015873c66e8a90537
AbhinashJha342 <abhinashjha09@gmail.com>
2022-09-22 22:22:41 -0500

    first commit

--- src/main/java/org/choicehotels/springrest/controller/HotelController.java
+++ src/main/java/org/choicehotels/springrest/controller/HotelController.java
@@ -1,2 +1,15 @@
-package org.choicehotels.springrest.controller;public class HotelController {
+package org.choicehotels.springrest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+
+    @GetMapping("/")
+    public String getEmployees() {
+        return "Hello!";
+    }
 }
